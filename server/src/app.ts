00f8fabc255dad0b8ca84e72e2c6e3e227e70ed1
vornsami <sami.vornanen@helsinki.fi>
2023-10-09 01:32:28 +0300

    not functional yet, gives error code 415

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,18 @@
 import express from 'express'
 import { helloWorldController } from './controllers/helloWorld'
+import * as llm from './services/largeLanguageModel'
 
 const app = express()
 
 app.get('/', helloWorldController)
+const prompt = 'Anwser this with nothing but "Hello World!"'
+
+async function async() {
+    await llm.query({"inputs": "The answer to the universe is"}).then((response) => {
+        console.log(JSON.stringify(response));
+    })
+    console.log(prompt)
+}
+async()
 
 export default app
