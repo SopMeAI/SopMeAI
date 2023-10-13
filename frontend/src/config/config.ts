0f4b93a655c28e30882c24a3f656870dc6c5cb8c
Nicholas <mickyaw1995@gmail.com>
2023-10-13 10:41:30 +0300

    removed dotenv and changed config.ts to match vitejs instructions

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,1 +1,1 @@
-export const GPT_API_URL = process.env.REACT_APP_GPT_API_URL || "http://localhost:3000/api/gpt";
+export const GPT_API_URL = import.meta.env.REACT_APP_GPT_API_URL || "http://localhost:3000/api/gpt";
