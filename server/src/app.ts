d53bd232dda68662b333fba1b4b5cf7e677f0c40
vornsami <sami.vornanen@hotmail.com>
2023-10-14 23:57:14 +0300

    Test additions

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,18 +1,8 @@
 import express from 'express'
 import { helloWorldController } from './controllers/helloWorld'
-import * as llm from './services/largeLanguageModel'
 
 const app = express()
 
 app.get('/', helloWorldController)
-const prompt = 'Anwser this with nothing but "Hello World!"'
-
-async function async() {
-    await llm.query({"inputs": "The answer to the universe is"}).then((response) => {
-        console.log(response[0].generated_text);
-    })
-    console.log(prompt)
-}
-async()
 
 export default app
--- server/src/services/largeLanguageModel.test.ts
+++ server/src/services/largeLanguageModel.test.ts
@@ -1,5 +1,7 @@
 import * as llm from './largeLanguageModel'
 
+
+
 describe('getHelloWorldMessage', () => {
   it('returns "Hello World!"', async () => {
     const prompt = 'Anwser this with nothing but "Hello World!"'
@@ -8,4 +10,18 @@ describe('getHelloWorldMessage', () => {
 
     expect(message[0].generated_text).toEqual('Hello World!')
   })
+
+  it('send long request', async () => {
+    const fs = require('fs')
+    const text = fs.readFile('../../test-data/lease-png-aws/rawText.txt', 'utf-8', (err:string,data:string) => {
+      if (err) throw err
+      return data
+    })
+    
+    const prompt = `Summarise information in the following agreement. The data was gotten through OCR, so there may be some missing data, so you may need to make some consessions. Here is the text: ${text}`
+    const completion = await llm.query({inputs:prompt})
+
+    expect(completion[0].generated_text).toEqual('')
+  })
 })
+
