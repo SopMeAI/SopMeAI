9d4b71b0d4082abca2356e6c96e671b676799636
vornsami <sami.vornanen@helsinki.fi>
2023-10-16 01:35:14 +0300

    OpenAI functionality

--- server/src/services/largeLanguageModel.test.ts
+++ server/src/services/largeLanguageModel.test.ts
@@ -3,25 +3,22 @@ import * as llm from './largeLanguageModel'
 
 
 describe('getHelloWorldMessage', () => {
-  it('returns "Hello World!"', async () => {
+  /*it('returns "Hello World!"', async () => {
     const prompt = 'Anwser this with nothing but "Hello World!"'
 
     const message = await llm.query({inputs: prompt})
 
-    expect(message[0].generated_text).toEqual('Hello World!')
-  })
+    expect(message[0].generated_text).toEqual('Hello World')
+  })*/
 
   it('send long request', async () => {
     const fs = require('fs')
-    const text = fs.readFile('../../test-data/lease-png-aws/rawText.txt', 'utf-8', (err:string,data:string) => {
-      if (err) throw err
-      return data
-    })
+    const text = fs.readFileSync('./test-data/lease-png-aws/rawText.txt', 'utf-8')
     
-    const prompt = `Summarise information in the following agreement. The data was gotten through OCR, so there may be some missing data, so you may need to make some consessions. Here is the text: ${text}`
-    const completion = await llm.query({inputs:prompt})
-
-    expect(completion[0].generated_text).toEqual('')
-  })
+    const prompt = `Hello! Could you please summarise information in the following agreement, and notify me if there are parts that are not in my favor. The data was gotten through OCR, so there may be some missing data, so you may need to make some consessions. Here is the text: ${text}`
+    const completion = await llm.gptquery(prompt)
+    console.log(prompt)
+    console.log(completion)
+  }, 100_000)
 })
 
--- server/src/services/largeLanguageModel.ts
+++ server/src/services/largeLanguageModel.ts
@@ -1,5 +1,7 @@
-import axios from 'axios'
+import { HfInference } from '@huggingface/inference'
+import OpenAI from "openai"
 require('dotenv').config()
+
 // interfaces
 interface Parameters {
     min_length?: number, // integer
@@ -21,32 +23,52 @@ interface Request {
     options?: Options
 }
 
-// Connection requirements
-const ENDPOINT = "https://api-inference.huggingface.co/models/google/flan-t5-xxl"
-const API_TOKEN = process.env.HUGGINGFACE_API_TOKEN
 
 // functions
-export async function query(data: Request) {
+export async function query(request: Request) {
+
+    // Connection requirements
+    const MODEL = 'google/flan-t5-xxl'
+    // const ENDPOINT = `https://api-inference.huggingface.co/models/${MODEL}`
+    const API_TOKEN = process.env.HUGGINGFACE_API_TOKEN
+    const hf = new HfInference(API_TOKEN)
+
+
     if (!API_TOKEN) return "Missing API TOKEN"
 
-    const config = {
+    /*const config = {
         headers: { 
             "Content-Type": "application/json",
             Authorization: `Bearer ${API_TOKEN}` 
         }
-    }
-    const body = JSON.stringify(data)
-    console.log(body)
+    }*/
 
-	return await axios.post(ENDPOINT, body, config)
-        .then((response) => {
-            console.log(response.data)
-            return response.data
-        })
-        .catch((error) => {
-            console.log('Error sending prompt')
-            throw (error)
-        }
-    )
-    return "Error returning completion"
+	return await hf.textGeneration({
+        model: MODEL,
+        inputs: request.inputs
+    }).then((response) => {
+        console.log(response.generated_text)
+        return response.generated_text
+    })
+    .catch((error) => {
+        console.log('Error sending prompt')
+        throw (error)
+    })
+}
+
+export async function gptquery(prompt: string) {
+    const API_TOKEN = process.env.OPENAI_API_TOKEN
+    const MODEL = 'gpt-3.5-turbo'
+
+    const openai = new OpenAI({
+        apiKey: API_TOKEN,
+    })
+
+    return await openai.chat.completions.create({
+        model: MODEL,
+        messages: [{ role: 'user', content: prompt }],
+    }).then((response:any) => {
+        return response.choices[0].message.content
+    })
+
 }
