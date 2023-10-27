d91b25e0c0d8e8d062eabf39429b1e6805f27cb8
rhiskk <hiski.r@gmail.com>
2023-10-27 13:25:31 +0300

    prompt template

--- server/src/services/largeLanguageModel.ts
+++ server/src/services/largeLanguageModel.ts
@@ -1,5 +1,4 @@
-import OpenAI from "openai"
-import {ChatCompletion} from 'openai/resources'
+import OpenAI from 'openai'
 
 import { env } from '../env'
 
@@ -8,21 +7,22 @@ const API_TOKEN = env.OPENAI_API_TOKEN
 const MODEL = 'gpt-3.5-turbo'
 
 // Initialises the default client
-const defaultClient =  new OpenAI({
-        apiKey: API_TOKEN,
-    })
+const defaultClient = new OpenAI({
+  apiKey: API_TOKEN,
+})
 
 // Makes a request to OpenAI's API
-export async function sendGPTQuery(prompt: string, client: OpenAI = defaultClient) {
-
-    const response = await client.chat.completions.create({
-        model: MODEL,
-        messages: [{ role: 'user', content: prompt }],
-    }).then((response:ChatCompletion) => {
-        return response
-    })
-
-    const completion = response.choices[0].message.content
-
-    return completion
+export async function sendGPTQuery(
+  prompt: string,
+  client: OpenAI = defaultClient,
+): Promise<string> {
+  const response = await client.chat.completions.create({
+    model: MODEL,
+    messages: [{ role: 'user', content: prompt }],
+  })
+  if (!response.choices[0].message.content) {
+    throw new Error('No response from GPT-3')
+  }
+  const completion = response.choices[0].message.content
+  return completion
 }
---
+++ server/src/services/promptService.test.ts
@@ -0,0 +1,18 @@
+import { generateContractPrompt } from './promptService'
+
+describe('generateContractPrompt', () => {
+  it('should generate a prompt with the correct prefix and data', () => {
+    const contractData = {
+      text: 'This is some contract text Checkbox 1 Checkbox 2',
+      checkboxes: [
+        { isSelected: true, label: 'Checkbox 1' },
+        { isSelected: false, label: 'Checkbox 2' },
+      ],
+    }
+    const prompt = generateContractPrompt(contractData)
+
+    expect(prompt).toContain('This is some contract text Checkbox 1 Checkbox 2')
+    expect(prompt).toContain('Checkbox 1 Selected: true')
+    expect(prompt).toContain('Checkbox 2 Selected: false')
+  })
+})
