2f5655c2cb25fb240f23d34aa077499b31c515f5
Ville Halonen <71012693+hzville@users.noreply.github.com>
2023-11-04 22:43:43 +0200

    add image upload from fronend to backend

--- server/src/controllers/helloWorld.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express'
-import { getHelloWorldMessage } from '../services/helloWorld'
-
-export function helloWorldController(_req: Request, res: Response) {
-  const message = getHelloWorldMessage()
-  res.send(message)
-}
