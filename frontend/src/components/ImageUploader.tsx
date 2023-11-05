488a0dde07c8542ae570f601ddfae5d3dd926d6e
rhiskk <hiski.r@gmail.com>
2023-11-05 12:00:49 +0200

    fix typescript errors

--- frontend/src/components/ImageUploader.tsx
+++ frontend/src/components/ImageUploader.tsx
@@ -64,7 +64,6 @@ const ImageUploader = () => {
             id="picture"
             type="file"
             accept="image/*"
-            encType="multipart/form-data"
             onChange={handleImageChange}
             className="mt-1 block w-full text-sm text-gray-500
                    file:mr-4 file:py-2 file:px-4
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,21 +1,20 @@
-import express from 'express'
+import express, { type Request } from 'express'
+import cors from 'cors'
+import multer from 'multer'
 
-const cors = require('cors')
-//Multer is used to handle image upload from frontend to backend
-const multer = require('multer')
 const upload = multer()
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.post('/api/aws/textract', upload.array('images'), (request, response) => {
+app.post('/api/aws/textract', upload.array('images'), (request: Request, response) => {
   /* upload.array('images') -> 'images' name is derived from the formData key/value pair, 
      e.g formData.append('images', image).
      Image(s) and other related data is found in the request.file header
      e.g 
      console.log('Uploaded image was', (request as any).files)
   */
-  console.log('Uploaded image was', (request as any).files)
+  console.log('Uploaded image was', request.files)
   response.send('file upload successful')
 })
 
