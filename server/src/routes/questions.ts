ec361b27903be2832164fd26d1376082a1da9b86
rhiskk <hiski.r@gmail.com>
2023-11-06 10:14:44 +0200

    async wrapper for express

--- server/src/routes/questions.ts
+++ server/src/routes/questions.ts
@@ -1,19 +1,22 @@
-import express, { Router, Request } from 'express'
-
+import { Router, Request } from 'express'
+import { runAsyncWrapper } from '../utils/runAsyncWrapper'
 import { sendGPTQuery } from '../services/largeLanguageModel'
 import { sendUpdates } from './sse'
 
 const questionRouter = Router()
 
-questionRouter.post('/', express.json(), async (req: Request, res) => {
-  const { prompt } = req.body as { prompt: string }
-  try {
-    const completion = await sendGPTQuery(prompt)
-    sendUpdates(completion)
-    res.status(204).end()
-  } catch (error) {
-    res.status(500).json({ error: error.message })
-  }
-})
+questionRouter.post(
+  '/',
+  runAsyncWrapper(async (req: Request, res) => {
+    const { prompt } = req.body as { prompt: string }
+    try {
+      const completion = await sendGPTQuery(prompt)
+      sendUpdates(completion)
+      res.status(204).end()
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+    }
+  }),
+)
 
 export default questionRouter
